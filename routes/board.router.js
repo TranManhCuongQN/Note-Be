c2c111ed0351a84f7231ca1a084a0fcd6759d531
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-06-20 22:59:47 +0700

    handle router

---
+++ routes/board.router.js
@@ -0,0 +1,58 @@
+import { param } from "express-validator";
+import { verifyToken } from "../middlewares/token.middleware.js";
+import { isObjectId, validate } from "../utils/validator.js";
+import express from "express";
+import {
+  createBoard,
+  getAllBoards,
+  getFavouriteBoards,
+  getOneBoard,
+  updateBoard,
+  updateFavouritePosition,
+  updatePosition,
+} from "../controllers/board.controller";
+
+const router = express.Router();
+
+router.post("/", verifyToken, createBoard);
+router.get("/", verifyToken, getAllBoards);
+router.put("/", verifyToken, updatePosition);
+router.get("/favourites", verifyToken, getFavouriteBoards);
+router.put("/favourites", verifyToken, updateFavouritePosition);
+router.get(
+  "/:boardId",
+  param("boardId").custom((value) => {
+    if (!isObjectId(value)) {
+      return Promise.reject("invalid id");
+    } else return Promise.resolve();
+  }),
+  validate,
+  verifyToken,
+  getOneBoard
+);
+
+router.put(
+  "/:boardId",
+  param("boardId").custom((value) => {
+    if (!isObjectId(value)) {
+      return Promise.reject("invalid id");
+    } else return Promise.resolve();
+  }),
+  validate,
+  verifyToken,
+  updateBoard
+);
+
+router.delete(
+  "/:boardId",
+  param("boardId").custom((value) => {
+    if (!isObjectId(value)) {
+      return Promise.reject("invalid id");
+    } else return Promise.resolve();
+  }),
+  validate,
+  verifyToken,
+  deleteBoard
+);
+
+export default router;
--- routes/section.router.js
+++ routes/section.router.js
@@ -1,13 +1,12 @@
-import { body, param } from "express-validator";
+import { param } from "express-validator";
 import { verifyToken } from "../middlewares/token.middleware.js";
 import { isObjectId, validate } from "../utils/validator.js";
-import {
-  createTask,
-  updatePossition,
-  deleteTask,
-  updateTask,
-} from "../controllers/task.controller.js";
 import express from "express";
+import {
+  createSection,
+  deleteSection,
+  updateSection,
+} from "../controllers/section.controller";
 
 const router = express.Router();
 
@@ -15,63 +14,46 @@ router.post(
   "/",
   param("boardId").custom((value) => {
     if (!isObjectId(value)) {
+      return Promise.reject("invalid id");
+    } else return Promise.resolve();
+  }),
+  validate,
+  verifyToken,
+  createSection
+);
+
+router.put(
+  "/:sectionId",
+  param("boardId").custom((value) => {
+    if (!isObjectId(value)) {
       return Promise.reject("invalid board id");
     } else return Promise.resolve();
   }),
-  body("sectionId").custom((value) => {
+  param("sectionId").custom((value) => {
     if (!isObjectId(value)) {
       return Promise.reject("invalid section id");
     } else return Promise.resolve();
   }),
   validate,
   verifyToken,
-  createTask
-);
-
-router.put(
-  "/update-position",
-  param("boardId").custom((value) => {
-    if (!isObjectId(value)) {
-      return Promise.reject("invalid board id");
-    } else return Promise.resolve();
-  }),
-  validate,
-  verifyToken,
-  updatePossition
+  updateSection
 );
 
 router.delete(
-  "/:taskId",
+  "/:sectionId",
   param("boardId").custom((value) => {
     if (!isObjectId(value)) {
       return Promise.reject("invalid board id");
     } else return Promise.resolve();
   }),
-  param("taskId").custom((value) => {
+  param("sectionId").custom((value) => {
     if (!isObjectId(value)) {
-      return Promise.reject("invalid task id");
+      return Promise.reject("invalid section id");
     } else return Promise.resolve();
   }),
   validate,
   verifyToken,
-  deleteTask
-);
-
-router.put(
-  "/:taskId",
-  param("boardId").custom((value) => {
-    if (!isObjectId(value)) {
-      return Promise.reject("invalid board id");
-    } else return Promise.resolve();
-  }),
-  param("taskId").custom((value) => {
-    if (!isObjectId(value)) {
-      return Promise.reject("invalid task id");
-    } else return Promise.resolve();
-  }),
-  validate,
-  verifyToken,
-  updateTask
+  deleteSection
 );
 
 export default router;
